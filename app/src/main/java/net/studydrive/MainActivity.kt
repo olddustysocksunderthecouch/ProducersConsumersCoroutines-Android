beae5d3e7024c47c07df56bb7153c8d9527a0558
Adrian Bunge <adrian@nonacreative.com>
2019-07-09 20:54:18 +0200

    feat: use newSingleThreadContext and massiveRun to increment a counter

--- app/src/main/java/net/studydrive/MainActivity.kt
+++ app/src/main/java/net/studydrive/MainActivity.kt
@@ -2,12 +2,34 @@ package net.studydrive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
+
+    var counter = 10;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val counterContext = newSingleThreadContext("CounterContext")
+        var counter = 0
+
+        fun main() = launch {
+            withContext(Dispatchers.Default) {
+                massiveRun {
+                    // confine each increment to a single-threaded context
+                    withContext(counterContext) {
+                        counter++
+                    }
+                }
+            }
+            println("Counter = $counter")
+        }
+
     }
+
 }
